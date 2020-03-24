3d0e277eb17890220d5f31fae5431d4aee506a89
Bart van de Klundert <b.vdklundert@outlook.com>
2020-03-24 14:53:52 +0100

    :lipstick: fixed s-card style to fit 2 cards

--- src/components/home/index.tsx
+++ src/components/home/index.tsx
@@ -1,10 +1,10 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import { connect } from "react-redux";
-import News from '../news/index';
+// import { Link } from "react-router-dom";
+// import { connect } from "react-redux";
+import News from "../news/index";
 
 interface Props {
-//   loggedIn?: boolean;
+  //   loggedIn?: boolean;
 }
 
 const Nav: React.FC<Props> = () => {
