d904cdff28f79d5e247b97b166f6e31037bf5fb9
Mike Hendriks <mikehen6@gmail.com>
2020-03-25 14:54:01 +0100

    :recycle: Cleanup of home component

--- src/components/home/index.tsx
+++ src/components/home/index.tsx
@@ -1,35 +1,18 @@
 import React from "react";
-// import { Link } from "react-router-dom";
-// import { connect } from "react-redux";
 import News from "../news/index";
 import Activities from "../activities/index";
 import Organisers from "../organisers/index";
+import Rules from "../rules/index";
 
-interface Props {
-  //   loggedIn?: boolean;
-}
-
-const Nav: React.FC<Props> = () => {
+const Nav: React.FC = () => {
   return (
     <div className="s-home">
-      <Organisers></Organisers>
-      <News></News>
-      <Activities></Activities>
+      <Organisers />
+      <News />
+      <Activities />
+      <Rules />
     </div>
   );
 };
 
-// const mapStateToProps = (state: any) => {
-//   if (state.firebase.auth.isEmpty === false) {
-//     return { loggedIn: true };
-//   } else {
-//     return { loggedIn: false };
-//   }
-// };
-// const mapDispatchToProps = (dispatch: any) => {
-//   return {
-//     signOut: () => dispatch(signOut())
-//   };
-// };
-// export default connect(mapStateToProps, mapDispatchToProps)(Nav);
 export default Nav;
