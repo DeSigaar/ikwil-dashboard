a80e66049889d6687ef68221048c6b44daf5bb3b
Bart van de Klundert <b.vdklundert@outlook.com>
2020-03-25 14:32:53 +0100

    :children_crossing: fixed scrollable only on activities not the header

--- src/components/activities/summary.tsx
+++ src/components/activities/summary.tsx
@@ -6,35 +6,42 @@ interface Props {
 }
 const Summary: React.FC<Props> = ({ data }) => {
   return (
-    <div className="s-card __activity-container">
+    // <div className="s-card __activity-container">
+    <>
       <h2 className="s-card__header">Activiteiten</h2>
-      {typeof data !== "undefined" ? (
-        <>
-          {data.map(activity => {
-            return (
-              <Link
-                key={activity.id}
-                to={"/activity/" + activity.id}
-                className="c-activity"
-              >
-                <div className="__top-content">
-                  <img className="__icon" src="/yoga.svg" alt="activity icon" />
-                  <h3>{activity.name}</h3>
-                </div>
-                <div className="__bottom-content">
-                  <div className="__time">
-                    {activity.startTime} - {activity.endTime}
+      <div className="s-card __activity-container">
+        {typeof data !== "undefined" ? (
+          <>
+            {data.map(activity => {
+              return (
+                <Link
+                  key={activity.id}
+                  to={"/activity/" + activity.id}
+                  className="c-activity"
+                >
+                  <div className="__top-content">
+                    <img
+                      className="__icon"
+                      src="/yoga.svg"
+                      alt="activity icon"
+                    />
+                    <h3>{activity.name}</h3>
                   </div>
-                  <div>{activity.room}</div>
-                </div>
-              </Link>
-            );
-          })}
-        </>
-      ) : (
-        <></>
-      )}
-    </div>
+                  <div className="__bottom-content">
+                    <div className="__time">
+                      {activity.startTime} - {activity.endTime}
+                    </div>
+                    <div>{activity.room}</div>
+                  </div>
+                </Link>
+              );
+            })}
+          </>
+        ) : (
+          <></>
+        )}
+      </div>
+    </>
   );
 };
 export default Summary;
