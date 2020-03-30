52ddc0b3cc1d0f4927d1bf95a8535db188bfe5a7
Mike Hendriks <mikehen6@gmail.com>
2020-03-31 01:13:02 +0200

    :sparkles: :lipstick: base styling of meal modal done

--- src/components/meals/summary.tsx
+++ src/components/meals/summary.tsx
@@ -1,6 +1,8 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Carousel from "react-multi-carousel";
 import { Link } from "react-router-dom";
+import "react-multi-carousel/lib/styles.css";
+import Modal from "react-modal";
 import "react-multi-carousel/lib/styles.css";
 
 interface Props {
@@ -11,22 +13,27 @@ const responsive = {
   desktop: {
     breakpoint: { max: 3000, min: 1024 },
     items: 1,
-    slidesToSlide: 1 // optional, default to 1.
-  },
-  tablet: {
-    breakpoint: { max: 1024, min: 464 },
-    items: 1,
-    slidesToSlide: 1 // optional, default to 1.
-  },
-  mobile: {
-    breakpoint: { max: 464, min: 0 },
-    items: 1,
-    slidesToSlide: 1 // optional, default to 1.
   }
 };
 
+
 const Summary: React.FC<Props> = ({ meals }) => {
   const [isMoving, setIsMoving] = useState<boolean>(false);
+  const [modalIsOpen, setIsOpen] = React.useState<boolean>(false);
+  // const [modalContent, setModalContent] = React.useState<any>(false);
+  Modal.setAppElement('#root')
+
+  // useEffect(() => {
+  //   setModalContent(meals)
+  // }, [meals])
+
+  const closeModal = () => {
+    setIsOpen(false);
+  }
+
+  const openModal = () => {
+    setIsOpen(true);
+  }
 
   return (
     <div className="s-card-small" id="meal">
@@ -48,13 +55,12 @@ const Summary: React.FC<Props> = ({ meals }) => {
           >
             {meals.map((meal: iMeal) => {
               return (
-                <Link
+                <div
                   className="c-meal__link"
-                  key={1}
-                  to={"/meal/" + meal.id}
+                  key={meal.id}
                   onClick={e => {
-                    if (isMoving === true) {
-                      e.preventDefault();
+                    if (isMoving !== true) {
+                      openModal()
                     }
                   }}
                 >
@@ -66,7 +72,7 @@ const Summary: React.FC<Props> = ({ meals }) => {
                     />
                     <h2 className="c-meal__title">{meal.name}</h2>
                   </div>
-                </Link>
+                </div>
               );
             })}
           </Carousel>
@@ -74,6 +80,51 @@ const Summary: React.FC<Props> = ({ meals }) => {
       ) : (
         <></>
       )}
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+        style={{
+          overlay: {
+            backgroundColor: "rgba(0, 0, 0, .75)"
+          }
+        }}
+      >
+        <div className="ReactModal__Content__close-icon" onClick={closeModal}></div>
+        {typeof meals !== "undefined" ? (
+          <>
+            <div className="ReactModal__Content__image-wrapper">
+              <img
+                className="ReactModal__Content__image"
+                src="https://images.pexels.com/photos/35661/pasta-cheese-egg-food.jpg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+                alt="toiletrolls-Coronavirus"
+                />
+            </div>
+            <div className="ReactModal__Content__wrapper">
+              {meals.map((meal: iMeal) => {
+                return (
+                  <div className="c-modal-meal" key={meal.id}>
+                    <h2 className="c-modal-meal__title">{meal.name}</h2>
+                    <div className="c-modal-meal__left-wrapper">
+                      <div className="c-modal-meal__price">&euro;{meal.price}</div>
+                      <h3 className="c-modal-meal__ingredients-title">Ingredienten</h3>
+                      <div className="c-modal-meal__ingredients">{meal.ingredients}</div>
+                    </div>
+                    <div className="c-modal-meal__characteristics">
+                      <div className="c-modal-meal__characteristics__halal">{meal.isHallal ? "yes" : "no"}</div>
+                      <div className="c-modal-meal__characteristics__vegan">{meal.isVegan ? "yes" : "no"}</div>
+                      <div className="c-modal-meal__characteristics__vegetarian">{meal.isVegetarian ? "yes" : "no"}</div>
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
+          </>
+        ) : (
+          <></>
+        )}
+        {/* <h2 className="ReactModal__Content__title">{modalContent.title}</h2>
+        <p className="ReactModal__Content__text">{modalContent.text}</p> */}
+      </Modal>
     </div>
   );
 };
