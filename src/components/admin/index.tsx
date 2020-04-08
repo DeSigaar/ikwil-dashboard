943c7352fd4a4493218f935896177c13b4a972ce
Mike Hendriks <mikehen6@gmail.com>
2020-04-08 17:19:30 +0200

    :lipstick: Admin home page content added, issue #55

--- src/components/admin/index.tsx
+++ src/components/admin/index.tsx
@@ -1,5 +1,10 @@
 import React from "react";
 const Admin: React.FC = () => {
-  return <></>;
+  return (
+    <div className="c-adminHome">
+      <h1>Welkom</h1>
+      <p>Hier kun je de inhoud van het dashboard en de activiteiten van de app wijzigen.</p>
+    </div>
+  );
 };
 export default Admin;
