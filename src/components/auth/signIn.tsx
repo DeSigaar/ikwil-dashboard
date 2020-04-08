148d2c47a9b98032af0be2c40f031a15554935ed
Mark <mark.hendriks9999@gmail.com>
2020-04-08 10:42:08 +0200

    :bug: added redirect to login #5

--- src/components/auth/signIn.tsx
+++ src/components/auth/signIn.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { connect } from "react-redux";
 import { signIn } from "../../store/actions/authActions";
-
+import { Redirect } from "react-router-dom";
 interface Props {
   authError?: any;
   signIn: any;
@@ -10,53 +10,58 @@ interface Props {
 const SignIn: React.FC<Props> = ({ authError }) => {
   const [email, setEmail] = useState<string>("test@test.nl");
   const [password, setPassword] = useState<string>("testtest");
+  const [redirect, setRedirect] = useState<boolean>(false);
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-
     signIn({ email, password });
+    setRedirect(true);
   };
-  return (
-    <div className="s-login">
-      <img
-        src="/logo.svg"
-        alt="Stichting Ik Wil Logo"
-        className="s-login__logo"
-      />
-      <form onSubmit={e => handleSubmit(e)}>
-        <div className="o-inputfield">
-          <label>Emailadres</label>
-          <input
-            className="o-inputfield__input login"
-            required
-            value={email}
-            onChange={e => setEmail(e.target.value)}
-          />
-        </div>
-        <div className="o-inputfield">
-          <label>Wachtwoord</label>
-          <input
-            className="o-inputfield__input login"
-            required
-            type="password"
-            value={password}
-            onChange={e => setPassword(e.target.value)}
-          />
-        </div>
-        <button>Login</button>
-        {typeof authError !== "undefined" && authError !== null
-          ? authError.message
-          : null}
-      </form>
-    </div>
-  );
+  if (!redirect) {
+    return (
+      <div className="s-login">
+        <img
+          src="/logo.svg"
+          alt="Stichting Ik Wil Logo"
+          className="s-login__logo"
+        />
+        <form onSubmit={(e) => handleSubmit(e)}>
+          <div className="o-inputfield">
+            <label>Emailadres</label>
+            <input
+              className="o-inputfield__input login"
+              required
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div className="o-inputfield">
+            <label>Wachtwoord</label>
+            <input
+              className="o-inputfield__input login"
+              required
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <button>Login</button>
+          {typeof authError !== "undefined" && authError !== null
+            ? authError.message
+            : null}
+        </form>
+      </div>
+    );
+  } else {
+    return <Redirect to={"/admin"} />;
+  }
 };
 const mapStateToProps = (state: any) => {
   return { authError: state.auth.authError };
 };
 const mapDispatchToProps = (dispatch: any) => {
   return {
-    signIn: (user: iLogin) => dispatch(signIn(user))
+    signIn: (user: iLogin) => dispatch(signIn(user)),
   };
 };
 
